5b7045cbcdc0595834151a6d58f9db4656259cdf
Johan <johan.emil.lundgren@gmail.com>
2014-10-10 12:25:09 +0200

    start

---
+++ main/GFXInterface.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GFXInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		
+		
+	}
+
+}
